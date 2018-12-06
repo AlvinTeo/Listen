1bf2eea95005b98dce50a12459b9543c7fa36aaa
alvinkhaimingteo <alvinteo9696@gmail.com>
2018-12-06 21:14:58 +0000

    floating action button, change background color

--- app/src/main/java/com/skr/listen/SearchActivity.java
+++ app/src/main/java/com/skr/listen/SearchActivity.java
@@ -1,15 +1,13 @@
 package com.skr.listen;
 
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.os.Bundle;
 
 public class SearchActivity extends AppCompatActivity {
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
     }
 }
